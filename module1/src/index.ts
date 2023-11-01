4b7009deb952c9d59aa1f27929ed686c38ce0667
mostafizurRahaman <fahim654326@gmail.com>
2023-11-02 00:42:22 +0600

    typecript module-1

---
+++ module1/src/index.ts
@@ -0,0 +1,7 @@
+let course: string = "Next Level Development";
+console.log(course);
+const b: string = "Mostafizur rahaman";
+const age: number = 10;
+const age1: number = 6;
+const c = age + age1;
+console.log(c, b, course, age1, age);
