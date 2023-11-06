e82e4d8537e9346db98217b3fa7f669e5d3032be
mostafizurRahaman <fahim654326@gmail.com>
2023-11-06 16:06:19 +0600

    learn instance of and is predicate

--- module3/3.4.js
+++ module3/3.4.js
@@ -1,7 +1,79 @@
 "use strict";
+// {
+//    // type guard  instance of :
+//    // create a animal:
+//    class Animal {
+//       name: string;
+//       species: string;
+//       sound: string;
+//       constructor(name: string, species: string, sound: string) {
+//          this.name = name;
+//          this.species = species;
+//          this.sound = sound;
+//       }
+//       makeSound() {
+//          console.log(`The ${this.name} says ${this.sound}`);
+//       }
+//    }
+//    // create a  children class for dog by extending Animal
+//    class Dog extends Animal {
+//       constructor(name: string, species: string, sound: string) {
+//          super(name, species, sound);
+//       }
+//       // make dog sound:
+//       makeBark() {
+//          console.log(`The ${this.name} is Barking `);
+//       }
+//    }
+//    // create a children class for cat extending Animal:
+//    class Cat extends Animal {
+//       constructor(name: string, species: string, sound: string) {
+//          super(name, species, sound);
+//       }
+//       // make sound for cat;
+//       makeMeaw() {
+//          console.log(`The ${this.name} says ${this.sound}`);
+//       }
+//    }
+//    // create an instance for dog
+//    const dog = new Dog("Shadow Dog", "dog", "gew gew");
+//    // create an instance for cat:
+//    const cat = new Dog("Black Cat", "cat", "meaw meaw");
+//    // create another instance for pig:
+//    const pig = new Animal("Pink Pig", "pig", "make some sweet sounds");
+//    // const getAnimalAndMakeSound = (animal: Animal) => {
+//    //    if (animal instanceof Dog) {
+//    //       animal.makeBark();
+//    //    } else if (animal instanceof Cat) {
+//    //       animal.makeMeaw();
+//    //    } else {
+//    //       animal.makeSound();
+//    //    }
+//    // };
+//    // getAnimalAndMakeSound(dog);
+//    // getAnimalAndMakeSound(cat);
+//    // getAnimalAndMakeSound(pig);
+//    // we can handle smartly handle check type by using function:
+//    const isDog = (animal: Animal): animal is Dog => {
+//       return animal instanceof Dog;
+//    };
+//    const isCat = (animal: Animal): animal is Cat => {
+//       return animal instanceof Cat;
+//    };
+//    const getAnimalAndMakeSound = (animal: Animal) => {
+//       if (isDog(animal)) {
+//          animal.makeBark();
+//       } else if (isCat(animal)) {
+//          animal.makeMeaw();
+//       } else {
+//          animal.makeSound();
+//       }
+//    };
+//    getAnimalAndMakeSound(cat);
+//    getAnimalAndMakeSound(dog);
+//    getAnimalAndMakeSound(pig);
+// }
 {
-    // type guard  instance of :
-    // create a animal:
     class Animal {
         constructor(name, species, sound) {
             this.name = name;
@@ -12,63 +84,65 @@
             console.log(`The ${this.name} says ${this.sound}`);
         }
     }
-    // create a  children class for dog by extending Animal
-    class Dog extends Animal {
-        constructor(name, species, sound) {
-            super(name, species, sound);
-        }
-        // make dog sound:
-        makeBark() {
-            console.log(`The ${this.name} is Barking `);
-        }
-    }
-    // create a children class for cat extending Animal:
     class Cat extends Animal {
         constructor(name, species, sound) {
             super(name, species, sound);
         }
-        // make sound for cat;
-        makeMeaw() {
+        makeMew() {
+            console.log(`The ${this.name} is meaw meaw`);
+        }
+    }
+    class Cow extends Animal {
+        constructor(name, species, sound) {
+            super(name, species, sound);
+        }
+        makeHamba() {
             console.log(`The ${this.name} says ${this.sound}`);
         }
     }
-    // create an instance for dog
-    const dog = new Dog("Shadow Dog", "dog", "gew gew");
-    // create an instance for cat:
-    const cat = new Dog("Black Cat", "cat", "meaw meaw");
-    // create another instance for pig:
-    const pig = new Animal("Pink Pig", "pig", "make some sweet sounds");
-    // we can handle smartly handle check type by using function:
-    const isDog = (animal) => {
-        return animal instanceof Dog;
-    };
-    const isCat = (animal) => {
-        return animal instanceof Cat;
-    };
-    const getAnimalAndMakeSound = (animal) => {
-        if (isDog(animal)) {
-            animal.makeBark();
+    // create instanceof cow :
+    const cow = new Cow("Red Cow", "cow", "hamba hamba");
+    // create instanceof cat:
+    const cat = new Cat("white cat", "cat", "mew mew");
+    console.log(cow, cat);
+    // create a pig form 'Animal instance'
+    const pig = new Animal("Pink pig", "pig", "sweet sounds");
+    //  get sound with instanceof operator :
+    const getAnimalSound = (animal) => {
+        if (animal instanceof Cat) {
+            animal.makeMew();
         }
-        else if (isCat(animal)) {
-            animal.makeMeaw();
+        else if (animal instanceof Cow) {
+            animal.makeHamba();
         }
         else {
             animal.makeSound();
         }
     };
+    getAnimalSound(cow);
+    getAnimalSound(cat);
+    getAnimalSound(pig);
+    // we can also do type guarding or narrowing by defining function
+    // here we need to sepecify with `is` predicate
+    const isCow = (animal) => {
+        return animal instanceof Cow;
+    };
+    const isCat = (animal) => {
+        return animal instanceof Cat;
+    };
+    const getAnimalAndMakeSound = (animal) => {
+        if (isCat(animal)) {
+            animal.makeMew();
+        }
+        else if (isCow(animal)) {
+            animal.makeHamba();
+        }
+        else {
+            animal.makeSound();
+        }
+    };
+    // call the getAnimalAndMakeSound function and get sounds;
     getAnimalAndMakeSound(cat);
-    getAnimalAndMakeSound(dog);
+    getAnimalAndMakeSound(cow);
     getAnimalAndMakeSound(pig);
 }
-// const getAnimalAndMakeSound = (animal: Animal) => {
-//    if (animal instanceof Dog) {
-//       animal.makeBark();
-//    } else if (animal instanceof Cat) {
-//       animal.makeMeaw();
-//    } else {
-//       animal.makeSound();
-//    }
-// };
-// getAnimalAndMakeSound(dog);
-// getAnimalAndMakeSound(cat);
-// getAnimalAndMakeSound(pig);
--- module3/3.4.ts
+++ module3/3.4.ts
@@ -1,7 +1,94 @@
-{
-   // type guard  instance of :
+// {
+//    // type guard  instance of :
 
-   // create a animal:
+//    // create a animal:
+//    class Animal {
+//       name: string;
+//       species: string;
+//       sound: string;
+
+//       constructor(name: string, species: string, sound: string) {
+//          this.name = name;
+//          this.species = species;
+//          this.sound = sound;
+//       }
+
+//       makeSound() {
+//          console.log(`The ${this.name} says ${this.sound}`);
+//       }
+//    }
+//    // create a  children class for dog by extending Animal
+//    class Dog extends Animal {
+//       constructor(name: string, species: string, sound: string) {
+//          super(name, species, sound);
+//       }
+
+//       // make dog sound:
+//       makeBark() {
+//          console.log(`The ${this.name} is Barking `);
+//       }
+//    }
+
+//    // create a children class for cat extending Animal:
+//    class Cat extends Animal {
+//       constructor(name: string, species: string, sound: string) {
+//          super(name, species, sound);
+//       }
+
+//       // make sound for cat;
+//       makeMeaw() {
+//          console.log(`The ${this.name} says ${this.sound}`);
+//       }
+//    }
+
+//    // create an instance for dog
+//    const dog = new Dog("Shadow Dog", "dog", "gew gew");
+
+//    // create an instance for cat:
+//    const cat = new Dog("Black Cat", "cat", "meaw meaw");
+
+//    // create another instance for pig:
+//    const pig = new Animal("Pink Pig", "pig", "make some sweet sounds");
+
+//    // const getAnimalAndMakeSound = (animal: Animal) => {
+//    //    if (animal instanceof Dog) {
+//    //       animal.makeBark();
+//    //    } else if (animal instanceof Cat) {
+//    //       animal.makeMeaw();
+//    //    } else {
+//    //       animal.makeSound();
+//    //    }
+//    // };
+
+//    // getAnimalAndMakeSound(dog);
+//    // getAnimalAndMakeSound(cat);
+//    // getAnimalAndMakeSound(pig);
+
+//    // we can handle smartly handle check type by using function:
+//    const isDog = (animal: Animal): animal is Dog => {
+//       return animal instanceof Dog;
+//    };
+
+//    const isCat = (animal: Animal): animal is Cat => {
+//       return animal instanceof Cat;
+//    };
+
+//    const getAnimalAndMakeSound = (animal: Animal) => {
+//       if (isDog(animal)) {
+//          animal.makeBark();
+//       } else if (isCat(animal)) {
+//          animal.makeMeaw();
+//       } else {
+//          animal.makeSound();
+//       }
+//    };
+
+//    getAnimalAndMakeSound(cat);
+//    getAnimalAndMakeSound(dog);
+//    getAnimalAndMakeSound(pig);
+// }
+
+{
    class Animal {
       name: string;
       species: string;
@@ -17,42 +104,56 @@
          console.log(`The ${this.name} says ${this.sound}`);
       }
    }
-   // create a  children class for dog by extending Animal
-   class Dog extends Animal {
-      constructor(name: string, species: string, sound: string) {
-         super(name, species, sound);
-      }
 
-      // make dog sound:
-      makeBark() {
-         console.log(`The ${this.name} is Barking `);
-      }
-   }
-
-   // create a children class for cat extending Animal:
    class Cat extends Animal {
       constructor(name: string, species: string, sound: string) {
          super(name, species, sound);
       }
 
-      // make sound for cat;
-      makeMeaw() {
+      makeMew() {
+         console.log(`The ${this.name} is meaw meaw`);
+      }
+   }
+
+   class Cow extends Animal {
+      constructor(name: string, species: string, sound: string) {
+         super(name, species, sound);
+      }
+
+      makeHamba() {
          console.log(`The ${this.name} says ${this.sound}`);
       }
    }
 
-   // create an instance for dog
-   const dog = new Dog("Shadow Dog", "dog", "gew gew");
+   // create instanceof cow :
+   const cow = new Cow("Red Cow", "cow", "hamba hamba");
 
-   // create an instance for cat:
-   const cat = new Dog("Black Cat", "cat", "meaw meaw");
+   // create instanceof cat:
+   const cat = new Cat("white cat", "cat", "mew mew");
+   console.log(cow, cat);
 
-   // create another instance for pig:
-   const pig = new Animal("Pink Pig", "pig", "make some sweet sounds");
+   // create a pig form 'Animal instance'
+   const pig = new Animal("Pink pig", "pig", "sweet sounds");
 
-   // we can handle smartly handle check type by using function:
-   const isDog = (animal: Animal): animal is Dog => {
-      return animal instanceof Dog;
+   //  get sound with instanceof operator :
+   const getAnimalSound = (animal: Animal) => {
+      if (animal instanceof Cat) {
+         animal.makeMew();
+      } else if (animal instanceof Cow) {
+         animal.makeHamba();
+      } else {
+         animal.makeSound();
+      }
+   };
+
+   getAnimalSound(cow);
+   getAnimalSound(cat);
+   getAnimalSound(pig);
+
+   // we can also do type guarding or narrowing by defining function
+   // here we need to sepecify with `is` predicate
+   const isCow = (animal: Animal): animal is Cow => {
+      return animal instanceof Cow;
    };
 
    const isCat = (animal: Animal): animal is Cat => {
@@ -60,30 +161,18 @@
    };
 
    const getAnimalAndMakeSound = (animal: Animal) => {
-      if (isDog(animal)) {
-         animal.makeBark();
-      } else if (isCat(animal)) {
-         animal.makeMeaw();
+      if (isCat(animal)) {
+         animal.makeMew();
+      } else if (isCow(animal)) {
+         animal.makeHamba();
       } else {
          animal.makeSound();
       }
    };
 
+   // call the getAnimalAndMakeSound function and get sounds;
+
    getAnimalAndMakeSound(cat);
-   getAnimalAndMakeSound(dog);
+   getAnimalAndMakeSound(cow);
    getAnimalAndMakeSound(pig);
 }
-
-// const getAnimalAndMakeSound = (animal: Animal) => {
-//    if (animal instanceof Dog) {
-//       animal.makeBark();
-//    } else if (animal instanceof Cat) {
-//       animal.makeMeaw();
-//    } else {
-//       animal.makeSound();
-//    }
-// };
-
-// getAnimalAndMakeSound(dog);
-// getAnimalAndMakeSound(cat);
-// getAnimalAndMakeSound(pig);
